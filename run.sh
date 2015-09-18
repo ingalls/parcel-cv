bba269df55e3e9c3594bb3bfe2e641566cadc1e7
ingalls <nick@mapbox.com>
2015-09-18 18:16:25 +0000

    Finished miller - start turner

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@
 # Seminole ✓    Clinch     ✓    Glynn    ✓    Cook     - 
 # Thomas   ✓    Ware       ✓    Brantley ✓    Colquitt -
 # Brooks   ✓    Pierce     ✓    Atkinson ✓    Mitchell -
-# Lowndes  ✓    Charlton   ✓    Lanier   ✓    Miller   -
+# Lowndes  ✓    Charlton   ✓    Lanier   ✓    Miller   ✓
 # Echols   ✓    Camden     -    Berrien  -    Early    -
 # Clay          Dougherty       Tift     -    Irwin    -
-# Calhoun       Worth           Turner        Coffee   -
+# Calhoun       Worth           Turner   -    Coffee   -
 # Ben Hill      Telfair         Lee           Randolph -
 # == Not Avaliable =======================================
 # Decatur
